7f4f3b30e900106b4ce6c49cba62dd563dfb6c96
feizheng <1290657123@qq.com>
2017-01-03 10:07:43 +0800

    intial

---
+++ index.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+
+alias dk='docker';
+alias dk-info='docker info';
+alias dk-ps='docker ps';
+alias dk-images='docker images';
+alias dk-run='docker run -it ';
+
+
+unset ROOT_PATH;
