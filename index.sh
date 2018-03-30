3d9ad919695c8726e37b54ec78178fc733d96238
afeiship <1290657123@qq.com>
2018-03-30 17:40:05 +0800

    issue: udpate docker compose

--- index.sh
+++ index.sh
@@ -2,11 +2,17 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
+## docker
 alias dk='docker';
 alias dk-info='docker info';
 alias dk-ps='docker ps';
 alias dk-images='docker images';
 alias dk-run='docker run -it ';
 
+## docker-compose
+alias dkc='docker-compose';
+alias dkc-down='docker-compose down';
+alias dkc-up='docker-compose up -d';
+alias dkc-reload='docker-compose down && docker-compose up -d';
 
 unset ROOT_PATH;
