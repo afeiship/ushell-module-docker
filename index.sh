8c693aab88e9114fdf7d3fe8cf160cf634e12826
afeiship <1290657123@qq.com>
2025-02-10 10:53:51 +0800

    feat: add colima

--- index.sh
+++ index.sh
@@ -1,29 +1,35 @@
 #!/usr/bin/env bash
 ## path:
-ROOT_PATH=$(dirname $BASH_SOURCE);
+ROOT_PATH=$(dirname $BASH_SOURCE)
 
 ## docker
-alias dk='docker';
-alias dk-pull='docker pull';
-alias dk-push='docker push';
-alias dk-info='docker info';
-alias dk-ps='docker ps';
-alias dk-rm='docker rmi -f';
-alias dk-ls='docker images';
-alias dk-mv="~/bin/dkmv.sh";
-alias dk-run='docker run -it ';
-alias dk-exec='docker exec -it';
+alias dk='docker'
+alias dk-pull='docker pull'
+alias dk-push='docker push'
+alias dk-info='docker info'
+alias dk-ps='docker ps'
+alias dk-rm='docker rmi -f'
+alias dk-ls='docker images'
+alias dk-mv="~/bin/dkmv.sh"
+alias dk-run='docker run -it '
+alias dk-exec='docker exec -it'
 
 ## docker-compose
-alias dkc='docker-compose';
-alias dkc-ps='docker-compose ps';
-alias dkc-stop='docker-compose down';
-alias dkc-start='docker-compose up';
-alias dkc-start-d='docker-compose up -d';
-alias dkc-restart='docker-compose down && docker-compose up -d';
-alias dkc-up='docker-compose up';
-alias dkc-dw='docker-compose down';
-alias dkc-upd='docker-compose up -d';
-alias dkc-re='docker-compose down && docker-compose up -d';
+alias dkc='docker-compose'
+alias dkc-ps='docker-compose ps'
+alias dkc-stop='docker-compose down'
+alias dkc-start='docker-compose up'
+alias dkc-start-d='docker-compose up -d'
+alias dkc-restart='docker-compose down && docker-compose up -d'
+alias dkc-up='docker-compose up'
+alias dkc-dw='docker-compose down'
+alias dkc-upd='docker-compose up -d'
+alias dkc-re='docker-compose down && docker-compose up -d'
 
-unset ROOT_PATH;
+## colima
+alias cl-start='colima start'
+alias cl-stop='colima stop'
+alias cl-restart='colima restart'
+alias cl-status='colima status'
+
+unset ROOT_PATH
